b7065ac09935762acc33e50c05c62f598e05676a
umeshror <sarukumesh@gmail.com>
2020-07-13 15:05:16 +0530

    changed regions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'auth-service-dev.us-west-2.elasticbeanstalk.com'
+  apiUrl: 'http://auth-service-dev.us-west-2.elasticbeanstalk.com'
 };
