03a0c3821d8216009c0c2ff9ed74b531c1696a1c
umeshror <sarukumesh@gmail.com>
2020-07-15 11:38:37 +0530

    google componenent added

--- src/app/core/login/google-login/google-login.component.ts
+++ src/app/core/login/google-login/google-login.component.ts
@@ -1,11 +1,7 @@
-import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
-import {AuthService} from "../../auth";
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {environment} from "../../../../environments/environment";
-import {MatIconRegistry} from "@angular/material/icon";
-import {DomSanitizer} from "@angular/platform-browser";
 
 declare const gapi: any;
-
 @Component({
   selector: 'app-google-login',
   templateUrl: './google-login.component.html',
@@ -19,49 +15,37 @@ export class GoogleLoginComponent implements OnInit, AfterViewInit {
     'email'
   ].join(' ');
 
-  private googleLogoURL = "https://raw.githubusercontent.com/fireflysemantics/logo/master/Google.svg";
   public auth2: any;
 
-  constructor(
-    private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer,
-    private element: ElementRef,
-    private authService: AuthService
-  ) {
-    this.matIconRegistry.addSvgIcon('logo',
-      this.domSanitizer.bypassSecurityTrustResourceUrl(this.googleLogoURL));
+  constructor() {
   }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    this.googleInit();
+    this.googleOnInit()
   }
 
-
-  public googleInit(): void {
-    gapi.load('auth2', () => {
-      this.auth2 = gapi.auth2.init({
-        client_id: this.clientId,
-        cookiepolicy: 'single_host_origin',
-        scope: this.scope
-      });
-      this.attachSignin(this.element.nativeElement.firstChild);
+  public googleOnInit(): void{
+        gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': this.onSignIn,
+      'onfailure': this.onFailure
     });
   }
+  public onSignIn(googleUser): void {
+    console.log('googleUser');
+    console.log(googleUser);
+    console.log(googleUser.getBasicProfile())
+  }
 
-  public attachSignin(element): void {
-    this.auth2.attachClickHandler(element, {},
-      (googleUser) => {
-        const profile = googleUser.getBasicProfile();
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('ID: ' + profile.getId());
-        console.log(profile);
-        // ...
-      }, error => {
-        console.log(JSON.stringify(error, undefined, 2));
-      });
+  public onFailure(error): void {
+    console.log(error);
   }
 }
 
